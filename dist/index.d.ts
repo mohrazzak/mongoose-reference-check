a141e3bf491a1529ffbc1c111c94bc781300d98c
Mohammad Abdalrazzak <xxiimonsteriixx20@gmail.com>
2025-12-14 19:01:43 +0300

    Refactor package.json to include scoped package name and update version to 2.0.1. Adjusted formatting for consistency.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
-import { Schema, Document } from 'mongoose';
-import type { ReferenceCheckOptions } from './types';
-export type { ReferenceCheckOptions, RefField, ValidationResult, RefModel } from './types';
+import { Schema, Document } from "mongoose";
+import type { ReferenceCheckOptions } from "./types";
+export type { ReferenceCheckOptions, RefField, ValidationResult, RefModel, } from "./types";
 declare function ReferenceCheck<T extends Document>(schema: Schema<T>, options?: ReferenceCheckOptions): void;
 export default ReferenceCheck;
 //# sourceMappingURL=index.d.ts.map
